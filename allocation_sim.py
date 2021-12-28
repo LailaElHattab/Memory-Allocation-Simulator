32b44d03a6940e0f754393c3434737b7c16e155d
basmadessouky <basma1900957@miuegypt.edu.eg>
2021-12-28 19:53:54 +0200

    final commit

--- allocation_sim.py
+++ allocation_sim.py
@@ -51,3 +51,25 @@ elif choice == 2:
 elif choice == 3:
     ans1, ans2 = methods.worstFit(
         block_num, block_size, process_num, process_size)
+
+if choice == 2 or choice == 3:
+    index = []
+    for w in range(0, len(ans1), 1):
+        for l in range(0, len(block_size), 1):
+            if ans1[w] == block_size[l]:
+                index.append(l)
+    ans1 = index
+# prints info in tabular form
+print("Process no.  Process size    Block no.   Block size")
+for i in range(0, len(ans2), 1):
+    print("      "+str(ans2[i]+1)+"           "+str(process_size[ans2[i]]
+                                                    )+"             "+str(ans1[i]+1)+"             "+str(block_size[ans1[i]])+"                   "+str(block_size[ans1[i]]-process_size[ans2[i]])+" units of block no. "+str(ans1[i]+1)+" is wasted")
+
+    # prints the process that'll need to wait
+print("Processes of the below sizes will have to wait:")
+for z in range(0, process_num, 1):
+    if z not in ans2:
+        print("p"+str(z+1)+" : "+str(process_size[z]))
+
+    # when num>num2 or vice versa
+    # if i have 2 processes of the same size
